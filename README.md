983cbf9639c59bc9a800e1850848f51c9f17cf87
Duong Dac <ddac@google.com>
2020-07-16 17:38:08 +0200

    Add plot for multiple values of m

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # webp-rdplot
 
+## Prerequisites
+- [libwebp](https://github.com/webmproject/libwebp)
+- [matplotlib](https://matplotlib.org/)
+- [Pillow](https://pillow.readthedocs.io/en/stable/)
+
 ## Usage
 
 ```$ python3 main.py filename```
--- main.py
+++ main.py
@@ -1,32 +1,46 @@
+import os
 import subprocess
 import sys
+
 import matplotlib.pyplot as plt
 
 from PIL import Image
 
 
-def process(filename, q):
+def process_q(filename, q, m):
     """Returns the file size and PSNR value for compression factor q."""
-    cmd = 'cwebp -q {0} -print_psnr -short -mt {1} -o {1}.webp'.format(
-        q, filename)
+    cmd = 'cwebp -q {0} -m {1} -print_psnr -short -mt {2} -o {2}.webp'.format(
+        q, m, filename)
     completed = subprocess.run(cmd, shell=True, capture_output=True)
+    subprocess.call(['rm', '{}.webp'.format(filename)])
     output = str(completed.stderr)
     info = output[2:-3].strip().split(' ')
     return int(info[0]), float(info[1])
+
+
+def process_m(filename, m, pixel_count):
+    """Returns the list of file sizes and PSNR values for 
+    compression method m.
+    """
+    bitrate, psnr = [], []
+    for q in range(0, 101, 5):
+        _size, _psnr = process_q(filename, q, m)
+        bitrate.append(_size / pixel_count)
+        psnr.append(_psnr)
+    return bitrate, psnr
 
 
 def main():
     filename = sys.argv[1]
     im = Image.open(filename)
     w, h = im.size
-    bitrate, psnr = [], []
+    methods = [0, 2, 4, 6]
 
-    for q in range(0, 101, 5):
-        _size, _psnr = process(filename, q)
-        bitrate.append(_size / (w * h))
-        psnr.append(_psnr)
-    
-    plt.plot(bitrate, psnr)
+    for m in methods:
+        bitrate, psnr = process_m(filename, m, w * h)
+        plt.plot(bitrate, psnr, label='m={}'.format(m))
+
+    plt.legend(loc='lower right')
     plt.xlabel('Bitrate')
     plt.ylabel('PSNR')
     plt.show()
