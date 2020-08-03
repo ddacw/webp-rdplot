19c9b33134f134e4e625dfb802127701a67f21e9
Duong Dac <dacduong07@gmail.com>
2020-08-04 01:25:39 +0200

    Refactor.

--- README.md
+++ README.md
@@ -13,6 +13,17 @@
 ### Display RD curves for a directory containing images
 ```$ python multi.py directory [psnr/ssim]```
 
+## Extras
+
+### `get_frames.py`
+**Requirements**: numpy, pytube3, moviepy
+
+```$ python get_frames.py {url} {outfile} [{resolution} {start} {end} {fps}]```
+
+**Example:** 
+`$ python get_frames.py https://youtu.be/g4Hbz2jLxvQ spi1 480p 79 89 6`
+
+
 ## Example
 
 ### `psnr.py`
--- extra/get_frames.py
+++ extra/get_frames.py
@@ -10,45 +10,57 @@ from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from moviepy.editor import *
 
 
-def get_video(url, outfile, res):
-    """Downloads video from Youtube with given quality."""
-    yt = YouTube(url)
+class Getter:
+    def __init__(self, url, outfile, res="360p", start_time=0, end_time=10, fps=5):
+        self.url = url
+        self.outfile = outfile
+        self.res = res
 
-    videos = yt.streams.filter(
-        file_extension="mp4", progressive=True, resolution=res)
-    assert len(videos) > 0, "Invalid resolution."
+        # in seconds
+        self.start_time = start_time
+        self.end_time = end_time
 
-    if not os.path.exists("{}.mp4".format(outfile)):
-        videos[0].download(filename=outfile)
-        print("Downloaded.")
-    else:
-        print("File already existed.")
+        self.fps = fps
 
+    def get_video(self):
+        """Downloads video from Youtube with given quality."""
+        yt = YouTube(self.url)
+        videos = yt.streams.filter(
+            file_extension="mp4", progressive=True, resolution=self.res)
+        assert len(videos) > 0, "Video unavailable."
 
-def get_extract(outfile, start_time, end_time):
-    """Trims videos for specified duration (in seconds)."""
-    ffmpeg_extract_subclip("{}.mp4".format(
-        outfile), start_time, end_time, targetname="{}-trimmed.mp4".format(outfile))
+        if not os.path.exists("{}.mp4".format(self.outfile)):
+            videos[0].download(filename=self.outfile)
+            print("Downloaded.")
+        else:
+            print("File already existed.")
 
+    def get_extract(self):
+        """Trims videos for specified duration (in seconds)."""
+        ffmpeg_extract_subclip("{}.mp4".format(self.outfile), self.start_time,
+                               self.end_time, targetname="{}-trimmed.mp4".format(self.outfile))
 
-def get_frames(outfile, fps):
-    try:
-        os.mkdir(outfile)
-    except OSError:
-        "Creating directory failed."
+    def get_frames(self):
+        """Creates list of frames with timestamps + directory of frames."""
+        try:
+            os.mkdir(self.outfile)
+        except OSError:
+            print("Creating directory failed.")
 
-    with open("{}-list.txt".format(outfile), "w") as frame_list:
-        clip = VideoFileClip("{}-trimmed.mp4".format(outfile))
-        for t in enumerate(np.arange(0, clip.duration, 1/fps)):
-            frame_filename = "{0}/{0}_{1}.jpeg".format(outfile, t[0])
-            clip.save_frame(frame_filename, t[1])
-            frame_list.write("{} {}\n".format(frame_filename, int(t[1]*1000)))
+        with open("{}-list.txt".format(self.outfile), "w") as frame_list:
+            clip = VideoFileClip("{}-trimmed.mp4".format(self.outfile))
+            for i, t in enumerate(np.arange(0, clip.duration, 1/self.fps)):
+                frame_filename = "{0}/{0}_{1}.jpeg".format(self.outfile, i)
+                clip.save_frame(frame_filename, t)
+                frame_list.write("{} {}\n".format(frame_filename, int(t*1000)))
 
 
 def main():
-    # python get_frames.py {url} {outfile} {resolution} {start} {end} {fps}
-    url = sys.argv[1]
-    outfile = sys.argv[2]
+    assert len(sys.argv) >= 3, "Insufficient arguments."
+
+    url = sys.argv[1].strip()
+    outfile = sys.argv[2].strip()
+
     resolution = "360p"
     start_time = 0
     end_time = 5
@@ -56,17 +68,18 @@ def main():
 
     if len(sys.argv) >= 4:
         resolution = sys.argv[3].strip()
+        assert resolution in ["1080p", "720p", "480p",
+                              "360p", "240p", "144p"], "Invalid resolution."
     if len(sys.argv) >= 6:
         start_time = float(sys.argv[4])
         end_time = float(sys.argv[5])
     if len(sys.argv) >= 7:
         fps = int(sys.argv[6])
 
-    print(url, resolution, start_time, end_time, fps)
-
-    get_video(url, outfile, resolution)
-    get_extract(outfile, start_time, end_time)
-    get_frames(outfile, fps)
+    getter = Getter(url, outfile, resolution, start_time, end_time, fps)
+    getter.get_video()
+    getter.get_extract()
+    getter.get_frames()
 
 
 if __name__ == "__main__":
